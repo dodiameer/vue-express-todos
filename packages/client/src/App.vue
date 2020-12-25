7d62b0ee56369f591a5237b58e91c16c1d19cbda
= <=>
2020-12-25 14:00:19 +0300

    Added lerna

--- packages/client/src/App.vue
+++ packages/client/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <h1>Monorepo client</h1>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
-<script>
-const common = require("@vue-express-todos/common")
-export default {
-  name: 'App',
-  created() {
-    common()
-  }
-}
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from './components/HelloWorld.vue';
+
+@Component({
+  components: {
+    HelloWorld,
+  },
+})
+export default class App extends Vue {}
 </script>
 
 <style>
