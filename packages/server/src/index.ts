116fbaeab23afa139b1f2aaa25f2835302e7d1d0
= <=>
2020-12-25 14:25:04 +0300

    Added cors middleware

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,9 +1,11 @@
 import { IndexRes } from "@vue-express-todos/common"
 import express, { Request, Response } from "express"
 import logger from "./middleware/logger"
+import cors from "cors"
 
 const app = express()
 app.use(express.json())
+app.use(cors({ origin: "*" }))
 app.use(logger)
 
 app.get("/", (_req: Request, res: Response<IndexRes>) => {
