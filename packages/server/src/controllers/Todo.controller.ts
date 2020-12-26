8252f7bf8580cc9bf4d0087cba84f6f0a6fff35e
= <=>
2020-12-26 14:52:13 +0300

    Added delete endpoint

--- packages/server/src/controllers/Todo.controller.ts
+++ packages/server/src/controllers/Todo.controller.ts
@@ -6,27 +6,50 @@ import TodoModel, { TodoDocument } from "../models/Todo.model";
 const TodoController = express.Router();
 
 TodoController.get("/api/todos", async (_req: Req, res: Res) => {
-  const todos = await TodoModel.find()
-  const mapped = todos.map(mapTodo)
-  return res.json(mapped)
+  const todos = await TodoModel.find();
+  const mapped = todos.map(mapTodo);
+  return res.json(mapped);
 });
 
-TodoController.get("/api/todos/:id", async (req: Req<{id: TodoDocument["_id"]}>, res: Res) => {
-  const todo = await TodoModel.findById(req.params.id)
-  if (todo === null) {
-    return res.status(404)
+TodoController.get(
+  "/api/todos/:id",
+  async (req: Req<{ id: TodoDocument["_id"] }>, res: Res) => {
+    const todo = await TodoModel.findById(req.params.id);
+    if (todo === null) {
+      return res.status(404);
+    }
+    res.json(mapTodo(todo));
   }
-  res.json(mapTodo(todo))
-})
+);
 
-TodoController.post("/api/todos", async (req: Req<{}, {}, ITodo>, res: Res<ITodo>) => {
-  const { id, updatedAt, createdAt, ...cleanedTodo } = req.body;
-  const todoDocument = new TodoModel(cleanedTodo) 
-  todoDocument.save().then(document => {
-      return res.status(201).json(mapTodo(document))
-    }).catch(error => {
-      return res.status(400).json(error)
-    })
-});
+TodoController.post(
+  "/api/todos",
+  async (req: Req<{}, {}, ITodo>, res: Res<ITodo>) => {
+    const { id, updatedAt, createdAt, ...cleanedTodo } = req.body;
+    const todoDocument = new TodoModel(cleanedTodo);
+    todoDocument
+      .save()
+      .then((document) => {
+        return res.status(201).json(mapTodo(document));
+      })
+      .catch((error) => {
+        return res.status(400).json(error);
+      });
+  }
+);
 
-export default TodoController
+TodoController.delete(
+  "/api/todos/:id",
+  async (req: Req<{ id: TodoDocument["id"] }>, res: Res) => {
+    const { id } = req.params;
+    TodoModel.findByIdAndRemove(id, { useFindAndModify: false })
+      .then(() => {
+        return res.status(200).json({ success: true });
+      })
+      .catch((err) => {
+        return res.status(500).json(err);
+      });
+  }
+);
+
+export default TodoController;
